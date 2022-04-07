d1a3df270eebc9dada8bb4eb208df165516d3455
ab.chaursiya <abhishek.chaursiya@myn.global>
2022-04-07 15:07:39 +0530

    all done

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser"
-import Products from "./pages/productList/ProductList"
+import ProductList from "./pages/productList/ProductList"
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
+
 
 function App() {
   return (
@@ -21,14 +24,20 @@ function App() {
           <Route path="/users">
             <UserList />
           </Route>
-          <Route path="/products">
-            <Products />
-          </Route>
           <Route path="/user/:userId">
             <User />
           </Route>
           <Route path="/newUser">
             <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductList/>
+          </Route>
+          <Route path="/product/:productId">
+            <Product />
+          </Route>
+          <Route path="/newproduct">
+            <NewProduct />
           </Route>
         </Switch>
       </div>
